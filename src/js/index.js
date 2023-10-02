507aec2dff9952035e34f750b26c8168f120fb98
kaidocorsa <146669722+kaidocorsa@users.noreply.github.com>
2023-10-02 14:12:50 -0400

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const botoes = document.querySelectorAll(".botao");
+
+const personagens  = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click",
+    () => {
+
+
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+
+    botao.classList.add("selecionado");
+    
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+
+    personagens[indice].classList.add("selecionado");
+
+   });
+});
+
